12df4a6a3a3ad6142b869222d6a99f1ae52c313e
yirunzhou <yirunzhou@brandeis.edu>
2019-04-18 11:23:36 -0400

    passed db tests

--- app.rb
+++ app.rb
@@ -18,7 +18,6 @@ require 'redis'
 require 'sidekiq'
 require 'securerandom'
 require 'dotenv/load'
-require 'bunny'
 require 'securerandom'
 
 Dir["./models/*.rb"].each {|file| require file }
@@ -31,11 +30,6 @@ helpers do
   Dir["./helpers/*.rb"].each {|file| require file }
 end
 
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['SIDEKIQ_URL'] }
-end
-
 # init redis client, maybe put into another file for cleaness
 $followers_redis = RedisClient.new(ENV['FOLLOWERS_REDIS'])
 $leaders_redis = RedisClient.new(ENV['LEADERS_REDIS'])
@@ -44,26 +38,5 @@ $followers_redis.clear
 $leaders_redis.clear
 $timeline_redis.clear
 
-before do
-  pass if (%w[login register].include?(request.path_info.split('/').last)) \
-           || request.path_info.include?('test')\
-           || request.path_info.include?('loaderio-b2296ad8f5d2ab4dfcc4ce34a0d36fa8') \
-           || params['test_user']
-  if not logged_in?
-    if request.path_info.include?('api')
-      halt 401, {errors: 'not logged in'}.to_json
-    elsif request.get?
-      redirect '/login', 303
-    else
-      halt 401, 'not logged in'
-    end
-  end
-end
-
 # Apis
 Dir["./apis/*.rb"].each {|file| require file }
-
-get '/*' do
-  puts "logged in"
-  send_file File.expand_path('index.html', settings.public_folder)
-end
